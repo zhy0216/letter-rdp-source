2d3dad7449d5f30daf875a1aebd5dd2fe4343449
Yang <zhy0216@gmail.com>
2022-04-16 00:10:05 +0800

    refactor

--- src/parserCombinator.js
+++ src/parserCombinator.js
@@ -3,9 +3,8 @@ import {
   many,
   sequenceOf,
   str,
-  anyOfString,
   skip,
-  recursiveParser, pipeParsers, possibly, many1,
+  recursiveParser, pipeParsers, possibly,
 } from "arcsecond"
 import {
   betweenBraces,
@@ -18,103 +17,8 @@ import {
   whitespaceSurrounded
 } from "./basic";
 
-const expression = recursiveParser(() => choice([
-  unaryExpression,
-  logicalExpression,
-  relationExpression,
-  binaryExpression,
-  assignmentExpression,
-  literal,
-  identifier,
-]))
+import {assignmentExpression, expression, relationExpression} from "./expression"
 
-const logicOperator = choice([
-  str("&&"),
-  str("||"),
-])
-
-const binaryOperator = choice([
-  anyOfString("+-*/"),
-])
-
-const relationOperator = choice([
-  str(">="),
-  str("<="),
-  str("<"),
-  str(">"),
-  str("=="),
-  str("!="),
-])
-
-const makeBinaryExpression = operator => sequenceOf([
-  whitespaceSurrounded(choice([literal, identifier])),
-  many1(sequenceOf([
-    operator,
-    whitespaceSurrounded(choice([literal, identifier])),
-  ]))
-]).map(([initialTerm, rest]) => {
-  let left = initialTerm
-
-  for (const items of rest) {
-    const [operator, right] = items
-    left = {
-      type: "BinaryExpression",
-      left,
-      operator,
-      right
-    }
-  }
-
-  return left
-})
-
-
-// ignore precedence for now....
-const binaryExpression = makeBinaryExpression(binaryOperator)
-
-const relationExpression = makeBinaryExpression(relationOperator)
-
-const logicalExpression = sequenceOf([
-  whitespaceSurrounded(relationExpression),
-  whitespaceSurrounded(logicOperator),
-  whitespaceSurrounded(expression),
-]).map(r => ({
-  type: "LogicalExpression",
-  operator: r[1],
-  left: r[0],
-  right: r[2],
-}))
-
-const unaryExpression = sequenceOf([
-  anyOfString("-!"),
-  choice([literal, identifier]),
-]).map(r => ({
-  type: "UnaryExpression",
-  operator: r[0],
-  argument: r[1],
-}))
-
-const assignmentOperator = choice([
-  str("="),
-  str("-="),
-  str("+=")
-])
-
-const assignmentExpression = sequenceOf([
-  whitespaceSurrounded(identifier),
-  assignmentOperator,
-  whitespaceSurrounded(
-    expression
-  ),
-])
-  .map((r) => ({
-    type: "AssignmentExpression",
-    left: r[0],
-    operator: r[1],
-    right: r[2],
-  }))
-
-/** statement **/
 const statement = recursiveParser(() => whitespaceSurrounded(choice([
   returnStatement,
   expressionStatement,
